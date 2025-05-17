e8598760b99fed4aa460d1524ae60a87217a745b
Ashutosh Verma <vkrypto@gmail.com>
2025-05-17 14:38:12 +0530

    sycniong

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@ docker stack deploy --compose-file broker-stack.yml  kafka-broker
 
 
 withing broker container :
-kafka-broker-api-versions.sh --bootstrap-server kafka_broker:9092
+kafka-broker-api-versions.sh --bootstrap-server kafka-broker_kafka1:9092
 
-kafka-topics.sh --bootstrap-server kafka_broker:9092 --list
+kafka-topics.sh --bootstrap-server kafka-broker_kafka2:9092 --list
 
 
 check zookeeper node:
